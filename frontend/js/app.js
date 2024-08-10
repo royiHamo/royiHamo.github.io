93e0e459c5303e4c97f2fd396229b39200a29c73
Royi Hamo <royihamo@gmail.com>
2024-08-11 02:42:44 +0300

    update app.js

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -9,29 +9,6 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 });
 
-document.addEventListener("DOMContentLoaded", function() {
-    const loginForm = document.getElementById("loginForm");
-    if (loginForm) {
-        loginForm.addEventListener("submit", function(event) {
-            event.preventDefault();
-            const username = document.getElementById("username").value;
-            const password = document.getElementById("password").value;
-
-            if (username === "admin" && password === "NIRO8799!") {
-                window.location.href = "dashboard.html"; // Replace with the actual dashboard page
-            } else {
-                const errorMessage = document.getElementById("errorMessage");
-                if (errorMessage) {
-                    errorMessage.textContent = "שם משתמש או סיסמה שגויים.";
-                } else {
-                    console.info("Element with id 'errorMessage' not found.");
-                }
-            }
-        });
-    } else {
-        console.info("Element with id 'loginForm' not found.");
-    }
-});
 
 //on first page load use api/comments to get all the comments from the db bototel.db in backend folder
 fetch("http://localhost:3000/api/comments")
